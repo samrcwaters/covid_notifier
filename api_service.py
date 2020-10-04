aa447fdf43f07d16f91b5d0bb5cea881a593892f
Sam Waters <samrolandwaters@gmail.com>
2020-10-04 08:31:27 -0700

    date

--- api_service.py
+++ api_service.py
@@ -1,9 +1,27 @@
 import requests
 import json
+from datetime import date, timedelta
 
-r = requests.get('https://covid19.mathdro.id/api/daily/6-29-2020')
-data = json.loads(r.text)
+BASE_API_URL = 'https://covid19.mathdro.id/api/daily'
+NOT_FOUND_EXCEPTION = "Not Found"
 
-for o in data:
-  if o["admin2"] == "Yolo":
-    print(o)
+def lambda_handler(event, context):
+  today = date.today()
+  today_fmt = today.strftime("%m-%d-%y")
+  today_url = f'{BASE_API_URL}/{today_fmt}'
+  r = requests.get(today_url)
+
+  if r.reason == NOT_FOUND_EXCEPTION:
+    yesterday = today - timedelta(days=1)
+    yesterday_fmt = yesterday.strftime("%m-%d-%y")
+    yesterday_url = f'{BASE_API_URL}/{yesterday_fmt}'
+    r = requests.get(yesterday_url)
+
+  data = json.loads(r.text)
+
+  for o in data:
+    if o["admin2"] == "Sacramento":
+      print(o)
+
+if __name__ == "__main__":
+  lambda_handler(None, None)
