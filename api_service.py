20305a530413f17d106648509328905e5782fd61
Sam Waters <samrolandwaters@gmail.com>
2020-10-04 16:08:47 -0700

    send email with stats

--- api_service.py
+++ api_service.py
@@ -1,6 +1,7 @@
 import requests
 import json
 from datetime import date, timedelta
+from send_email import send_email
 
 BASE_API_URL = 'https://covid19.mathdro.id/api/daily'
 NOT_FOUND_EXCEPTION = "Not Found"
@@ -17,16 +18,20 @@ def lambda_handler(event, context):
     list(filter(lambda x: x["admin2"] in counties, data)),
     list(filter(lambda x: x["admin2"] in counties, yday_data))
   )
-  print(statistics)
+  send_email(
+    current_day['date'], 
+    current_day['info'],
+    statistics
+  )
 
 def get_statistics(today_data: dict, yesterday_data: dict) -> dict:
   stats = []
   for i in range(len(today_data)):
-    # print(int(today_data[i]['deaths']) - int(yesterday_data[i]['deaths']))
     stats.append({
       'county': today_data[i]['admin2'],
       'new_deaths': (int(today_data[i]['deaths']) - int(yesterday_data[i]['deaths'])),
       'new_cases': (int(today_data[i]['confirmed']) - int(yesterday_data[i]['confirmed'])),
+      'total_cases': today_data[i]['confirmed'],
       'last_update': today_data[i]['lastUpdate'],
     })
   return stats
@@ -38,19 +43,22 @@ def get_day_data(day: date):
 
 def get_last_two_days_data() -> dict:
   info = ""
+  curr_day = date.today().strftime("%m-%d-%y")
   today_r = get_day_data(date.today())
   yesterday_r = get_day_data(date.today() - timedelta(days=1))
 
   if today_r.reason == NOT_FOUND_EXCEPTION:
+    curr_day = (date.today() - timedelta(days=1)).strftime("%m-%d-%y")
     today_r, yesterday_r = yesterday_r, get_day_data(date.today() - timedelta(days=2))
-    info = "Data has not yet been provided for today. This is yesterday's data."
+    info = "PLEASE NOTE: Data has not yet been provided for today. This is yesterday's data."
 
   return {
     "text": today_r.text,
     "prev_day": {
       "text": yesterday_r.text
     },
-    "info": info
+    "info": info,
+    "date": curr_day
   }
 
 if __name__ == "__main__":
--- send_email.py
+++ send_email.py
@@ -1,58 +1,67 @@
 import boto3
 from botocore.exceptions import ClientError
 
-SENDER = "Sam Waters <samrolandwaters@gmail.com>"
-RECIPIENT = "samrolandwaters@gmail.com"
-AWS_REGION = "us-west-2"
-SUBJECT = "SES Test Email"
-BODY_TEXT = ("Amazon SES Test (Python)\r\n"
-             "This email was sent with Amazon SES using the "
-             "AWS SDK for Python (Boto)."
-            )
-BODY_HTML = """<html>
-<head></head>
-<body>
-  <h1>Amazon SES Test (SDK for Python)</h1>
-  <p>This email was sent with
-    <a href='https://aws.amazon.com/ses/'>Amazon SES</a> using the
-    <a href='https://aws.amazon.com/sdk-for-python/'>
-      AWS SDK for Python (Boto)</a>.</p>
-</body>
-</html>
-            """
-CHARSET = "UTF-8"
-
-# Create new SES resource and specify a region
-client = boto3.client('ses', region_name=AWS_REGION)
-
-# Try to send the email
-try:
-  response = client.send_email(
-    Destination={
-      'ToAddresses': [
-        RECIPIENT,
-      ],
-    },
-    Message={
-      'Body': {
-        'Html': {
-          'Charset': CHARSET,
-          'Data': BODY_HTML,
-        },
-        'Text': {
-          'Charset': CHARSET,
-          'Data': BODY_HTML,
-        },
-      },
-      'Subject': {
-        'Charset': CHARSET,
-        'Data': SUBJECT,
-      },
-    },
-    Source=SENDER
+def send_email(date, info, statistics):
+  SENDER = "Covid-19 Notifier <samrolandwaters@gmail.com>"
+  RECIPIENT = "samrolandwaters@gmail.com"
+  AWS_REGION = "us-west-2"
+  SUBJECT = f'Covid-19 Statistics for {date}'
+  BODY_HTML = (
+    '<html>'
+      '<head></head>'
+      '<body>'
+        f'<h1>{SUBJECT}</h1>'
+        f'{format_statistics(statistics)}'
+        f'<p><i>{info}</i></p>'
+      '</body>'
+    '</html>'
   )
-except ClientError as e:
-  print(e.response['Error']['Message'])
-else:
-  print("Email sent! Message ID:"),
-  print(response['MessageId'])
+  CHARSET = "UTF-8"
+
+  # Create new SES resource and specify a region
+  client = boto3.client('ses', region_name=AWS_REGION)
+
+  # Try to send the email
+  try:
+    response = client.send_email(
+      Destination={
+        'ToAddresses': [
+          RECIPIENT,
+        ],
+      },
+      Message={
+        'Body': {
+          'Html': {
+            'Charset': CHARSET,
+            'Data': BODY_HTML,
+          },
+          'Text': {
+            'Charset': CHARSET,
+            'Data': BODY_HTML,
+          },
+        },
+        'Subject': {
+          'Charset': CHARSET,
+          'Data': SUBJECT,
+        },
+      },
+      Source=SENDER
+    )
+  except ClientError as e:
+    print(e.response['Error']['Message'])
+  else:
+    print("Email sent! Message ID:"),
+    print(response['MessageId'])
+
+def format_statistics(stats) -> str:
+  formatted = ''
+  for s in stats:
+    formatted += (
+      f'<h3>{s["county"]}</h3>'
+      '<ul>'
+        f'<li>New cases: {s["new_cases"]}</li>'
+        f'<li>New deaths: {s["new_deaths"]}</li>'
+        f'<li>Total cases: {s["total_cases"]}</li>'      
+      '</ul>'
+    )
+  return formatted
